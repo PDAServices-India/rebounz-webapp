fb38cfa73a2f82c2fc496c970d926940b16626c2
Pradeep <pradeep.pg89@gmail.com>
2016-08-03 03:56:05 +0530

    Added methods for creating hash and salt.

--- src/main/java/com/rebounz/login/util/HashingUtils.java
+++ src/main/java/com/rebounz/login/util/HashingUtils.java
@@ -12,16 +12,16 @@ import javax.crypto.spec.PBEKeySpec;
 import org.apache.commons.codec.binary.Base64;
 
 public final class HashingUtils {
-	
+
 	private HashingUtils() {
-		
+
 	}
-	
+
 	private static final int ITERATIONS = 65536;
-	
+
 	private static final int SIZE = 256;
-	
-	private static final String SHA_512 = "PBKDF2WithHmacSHA512";
+
+	private static final String SHA_1 = "PBKDF2WithHmacSHA1";
 
 	public static String generateRandomSalt() {
 		SecureRandom random = new SecureRandom();
@@ -31,17 +31,30 @@ public final class HashingUtils {
 	}
 
 	public static boolean comparePasswordHashes(String password, String salt, String passwordHash) {
-		
-		KeySpec spec = new PBEKeySpec(password.toCharArray(), Base64.decodeBase64(salt), 
-				ITERATIONS, SIZE);
+		String generatedHash = "";
 		try {
-			SecretKeyFactory skf = SecretKeyFactory.getInstance(SHA_512);
-			SecretKey key = skf.generateSecret(spec);
-			byte[] hash = key.getEncoded();
+			generatedHash = generateHash(password, salt);
 		} catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
 			return false;
 		}
-		
-		return false;
+		//System.out.println("Generated Hash: " + generatedHash);
+		//System.out.println("Original Hash: " + passwordHash);
+		return passwordHash.equals(generatedHash);
 	}
+
+	public static String generateHash(String password, String salt)
+			throws NoSuchAlgorithmException, InvalidKeySpecException {
+		KeySpec spec = new PBEKeySpec(password.toCharArray(), Base64.decodeBase64(salt), ITERATIONS, SIZE);
+		SecretKeyFactory skf = SecretKeyFactory.getInstance(SHA_1);
+		SecretKey key = skf.generateSecret(spec);
+		return Base64.encodeBase64String(key.getEncoded());
+	}
+
+	/*public static void main(String[] args) throws NoSuchAlgorithmException, InvalidKeySpecException {
+		String salt = generateRandomSalt();
+		String password = "Password123#";
+		String passwordHash = generateHash(password, salt);
+		System.out.println("Test 1 : " + comparePasswordHashes(password, salt, passwordHash));
+		System.out.println("Test 2 : " + comparePasswordHashes("password123#", salt, passwordHash));
+	}*/
 }
